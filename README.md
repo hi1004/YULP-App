578e235625bb2747bd23d8af89139e128f5c027d
OhByeongHeon <hi1004@github.com>
2022-07-14 17:23:36 +0900

    UPDATE README.md & butify

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # YULP
 
-[DEMO](https://yulp-app.vercel.app/)
+> YULP リニューアルサイト
+>
+> [DEMO](https://bit.ly/3PdYaIR)
+
+</br>
+
+## 1. 製作期間 ＆ 参加人数
+
+- 2020 年 （約 3 週）
+- 参加人数：1 人
+
+</br>
+
+## 2. 使った技術
+
+#### `Front-End`
+
+- HTML5
+- CSS3
+- jquery
+- Slick
+- Parallax
+- ScrollMagic
+
+<br>
+
+## 3. コア機能
+
+- Slick ライブラリーでスライド実装。
+- Loading ページ実装。
+- Parallax ライブラリーを使い、Parallax 効果実装。
+- ScrollMagic ライブラリーを使い、scroll の時、画面転換評価実装。
+
+<details>
+	<summary><b>コードで見るコア機能</b></summary>
+  
+  ### 3.1. スライド
+  - **Slickライブラリー** :pushpin: [コード確認](https://github.com/hi1004/YULP-App/blob/main/index.html#L21-L50)
+  ### 3.1. Loading
+  - **Loading** :pushpin: [コード確認](https://github.com/hi1004/YULP-App/blob/main/index.html#L21-L50)
+</details>
+<br>
+
+## 4. 成果 & 感じたこと
--- js/main.js
+++ js/main.js
@@ -1,58 +1,48 @@
 $(document).ready(function () {
-    
-    var button_click = 0;
-    $('#ham_btn').on('click', function (event) {
-        event.preventDefault();
+  var button_click = 0;
+  $('#ham_btn').on('click', function (event) {
+    event.preventDefault();
 
-        $(this).toggleClass('on');
-        $('.fullpageMenu').toggleClass('visibility'); 
-        button_click++;
+    $(this).toggleClass('on');
+    $('.fullpageMenu').toggleClass('visibility');
+    button_click++;
 
-        if (0 == button_click % 2) {
-            $('body').css({
-              
-                overflowY: 'visible'
-            });
-        } else {
-            $('body').css({
-             
-                overflowY: 'hidden'
-            });
-        }
-    });
-    
-    
+    if (0 == button_click % 2) {
+      $('body').css({
+        overflowY: 'visible',
+      });
+    } else {
+      $('body').css({
+        overflowY: 'hidden',
+      });
+    }
+  });
 });
 
 $(function () {
-    // init
-    var controller = new ScrollMagic.Controller();
+  // init
+  var controller = new ScrollMagic.Controller();
 
-    // section_5
-    var wipeAnimation = new TimelineMax()
-        // animate to second panel
-        .to("#pinContainer", 0.5, {
-            z: -150
-        }) // move back in 3D space
-        .to("#pinContainer", 1, {
-            x: "-25%"
-        }) // move in to first panel
-        .to("#pinContainer", 0.5, {
-            z: 0
-        }) 
+  // section_5
+  var wipeAnimation = new TimelineMax()
+    // animate to second panel
+    .to('#pinContainer', 0.5, {
+      z: -150,
+    }) // move back in 3D space
+    .to('#pinContainer', 1, {
+      x: '-25%',
+    }) // move in to first panel
+    .to('#pinContainer', 0.5, {
+      z: 0,
+    });
 
-    new ScrollMagic.Scene({
-            triggerElement: "#section_5",
-            triggerHook: "onLeave",
-            duration: "200%"
-        })
-        .setPin("#section_5")
-        .setTween(wipeAnimation)
-       /* .addIndicators() // add indicators (requires plugin)*/
-        .addTo(controller);
-    
-    
+  new ScrollMagic.Scene({
+    triggerElement: '#section_5',
+    triggerHook: 'onLeave',
+    duration: '200%',
+  })
+    .setPin('#section_5')
+    .setTween(wipeAnimation)
+    /* .addIndicators() // add indicators (requires plugin)*/
+    .addTo(controller);
 });
-
-
-
